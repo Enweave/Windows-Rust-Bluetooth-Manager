b2b39ce5e8be487733ba55289bfbf8175be2688a
Enweave <enweave@gmail.com>
2025-11-07 02:15:58 +0500

    tray-icon, instead?

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 #![windows_subsystem = "windows"]
 
-use tray_item::{IconSource, TrayItem};
+use tray_icon::menu::{Menu, MenuEvent, MenuItem};
+use tray_icon::{Icon, TrayIconBuilder};
+use winit::event_loop::{ControlFlow, EventLoopBuilder};
 
-enum Message {
-    Quit,
+enum UserEvent {
+    TrayIconEvent(tray_icon::TrayIconEvent),
+    MenuEvent(tray_icon::menu::MenuEvent),
 }
 
 fn main() {
-    let mut tray = TrayItem::new(
-        "Tray Example",
-        IconSource::Resource("assets/favicon.ico"),
-    ).unwrap();
+    let icon = Icon::from_path("../assets/favicon.ico", Some((32u32, 32u32))).unwrap();
+
+    let tray_icon = TrayIconBuilder::new()
+        .with_tooltip("system-tray - tray icon library!")
+        .with_icon(icon)
+        .build()
+        .unwrap();
+
+    let quit_item = MenuItem::new("Quit", true, None);
+
+    let tray_menu = Menu::new();
+    tray_menu.append_items(&[&quit_item]).unwrap();
+
+
+
 }
